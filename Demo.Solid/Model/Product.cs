c18309f1cbcafc47ee17f4aaa83f37201a69d650
João Henrique <joaohenriquetdj@gmail.com>
2021-11-12 12:19:28 -0300

    add Product

---
+++ Demo.Solid/Model/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Demo.Solid.Model
+{
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public decimal? Price { get; set; }
+        public Product(string name, decimal price)
+        {
+            Name = name ?? throw new ArgumentNullException();
+            Price = price;
+        }
+
+        public void Save()
+        {
+
+        }
+
+        public Product GetProduct()
+        {
+            return null;
+        }
+    }
+}
+
+
+
