02cbf1f23d28fc35d7c4b3704f23191635766a5e
João Henrique <joaohenriquetdj@gmail.com>
2021-11-12 11:41:31 -0300

    initial commit

---
+++ Demo.Solid/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo.Solid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
